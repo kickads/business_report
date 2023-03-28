2dcef14ea4a0d87d86f42fbac520e7276587ad20
hernan arica <hernanaricammm@gmail.com>
2023-03-28 09:18:59 -0300

    se agrega una mejora a la ui del badge

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -9,7 +9,7 @@ export function PublicLayout() {
     <>
       <header className="flex items-center justify-center relative">
         <img src={ logo } alt="Kickads" className="h-20 align-middle" />
-        { year && (<span className="absolute top-[50px] right-[280px] text-kickads text-xs font-medium">{ year }</span>) }
+        { year && (<span className="absolute top-[50px] right-[280px] px-3 py-1 text-kickads bg-kickads text-white rounded-xl text-xs font-medium">{ year }</span>) }
       </header>
       <main>
         <Wrapper>
